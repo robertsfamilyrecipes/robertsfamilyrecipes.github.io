fe8bc655aac3b899ae2155a97c79b6cfbca8add6
Mat Roberts <mat.dogmatic@gmail.com>
2026-02-01 16:51:49 +0000

    styling the index page

--- src/build-index.ps1
+++ src/build-index.ps1
@@ -3,7 +3,7 @@ Set-StrictMode -Version Latest
 
 $template = Get-Content "$PsScriptRoot\templates\index.html" -Raw
 
-$template = $template -replace '##LastUpdated##', (Get-Date -Format 'u')
+$template = $template -replace '##LastUpdated##', (Get-Date -Format 'yyyy-MM-dd HH:mm')
 
 $contents = Get-ChildItem -Path "$PsScriptRoot\..\docs\recipes" | ForEach-Object {
     $title = (Get-Content $_ | Select-String -Pattern '<title>(.*)</title>').Matches[0].Groups[1].Value
